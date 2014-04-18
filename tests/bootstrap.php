32f9d43bd2ba45779fb808b40b55592dc3358279
trunghieule <letrunghieu.cse09@gmail.com>
2014-04-18 11:30:40 +0700

    Support custom endpoint

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,28 @@
 <?php
 
-if (!file_exists(__DIR__ . "/../vendor/autoload.php")) {
-    die(
-        "\n[ERROR] You need to run composer before running the test suite.\n".
-        "To do so run the following commands:\n".
-        "    curl -s http://getcomposer.org/installer | php\n".
-        "    php composer.phar install\n\n"
-    );
+if (!file_exists(__DIR__ . "/../vendor/autoload.php"))
+{
+	die(
+			"\n[ERROR] You need to run composer before running the test suite.\n" .
+			"To do so run the following commands:\n" .
+			"    curl -s http://getcomposer.org/installer | php\n" .
+			"    php composer.phar install\n\n"
+	);
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\VCR\VCR::configure()->enableLibraryHooks(array('stream_wrapper'));
+$useCustomInfo	 = false;
+$testConfig		 = \Symfony\Component\Yaml\Yaml::parse('tests/xmlrpc.yml');
+if ($testConfig['endpoint'] && $testConfig['admin_login'] && $testConfig['admin_password'] && $testConfig['guest_login'] && $testConfig['guest_password'])
+{
+	$useCustomInfo = true;
+}
+if (!$useCustomInfo)
+{
+	\VCR\VCR::configure()->enableLibraryHooks(array('stream_wrapper'));
+}
+else
+{
+	VCR\VCR::onfigure()->enableLibraryHooks(array());
+}
