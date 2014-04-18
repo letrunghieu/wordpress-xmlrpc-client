eb92d552956a06806f4c01942751ef2ff199c1fb
trunghieule <letrunghieu.cse09@gmail.com>
2014-04-18 13:17:50 +0700

    Revert "Add Api document"
    This reverts commit e5b48729425a2af2ede6193e086438932dd838f2.

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","HieuLe\\WordpressXmlrpcClient\\Exception\\NetworkException"],["c","HieuLe\\WordpressXmlrpcClient\\Exception\\XmlrpcException"],["c","HieuLe\\WordpressXmlrpcClient\\WordpressClient"]];
--- vendor/bin/phpunit.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0/../phpunit/phpunit/composer/bin/phpunit
-php "%BIN_TARGET%" %*
