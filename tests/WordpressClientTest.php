6fb2e74de4d80921f97d93a9938ab44b06d2ece7
trunghieule <letrunghieu.cse09@gmail.com>
2014-04-18 10:50:17 +0700

    Update comments API test cases

--- tests/WordpressClientTest.php
+++ tests/WordpressClientTest.php
@@ -1049,4 +1049,226 @@ class WordpressClientTest extends \PHPUnit_Framework_TestCase
 		$result = $this->guestClient->setOptions(array('thumbnail_size_w' => 1000));
 	}
 
+	#
+	# Test comments API
+
+	#
+	
+	/**
+	 * @vcr comments/test-new-comment-vcr.yml
+	 */
+	public function testNewComment()
+	{
+		$posts		 = $this->client->getPosts(array('number' => 1));
+		$this->assertNotEmpty($posts);
+		$commentId	 = $this->client->newComment($posts[0]['post_id'], array('content' => 'Lorem ipsum 123'));
+		$this->assertGreaterThan(0, (int) $commentId);
+	}
+
+	/**
+	 * @vcr comments/test-new-conmment-no-post-exist-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 404
+	 * @expectedExceptionMessage Invalid post ID.
+	 */
+	public function testNewCommentNoPostExist()
+	{
+		$commentId = $this->client->newComment(1000, array('content' => 'First comment'));
+	}
+
+	/**
+	 * @vcr comments/test-get-comment-count-vcr.yml
+	 */
+	public function testGetCommentCount()
+	{
+		$posts		 = $this->client->getPosts(array('number' => 1));
+		$this->assertNotEmpty($posts);
+		$commentId	 = $this->client->newComment($posts[0]['post_id'], array('content' => 'Lorem ipsum 123 abc'));
+		$count		 = $this->client->getCommentCount($posts[0]['post_id']);
+		$this->assertArrayHasKey('approved', $count);
+		$this->assertArrayHasKey('awaiting_moderation', $count);
+		$this->assertArrayHasKey('spam', $count);
+		$this->assertArrayHasKey('total_comments', $count);
+	}
+
+	/**
+	 * @vcr comments/test-get-comment-count-no-privilege-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 403
+	 * @expectedExceptionMessage You are not allowed access to details about comments.
+	 */
+	public function testGetCommentCountNoPrivilege()
+	{
+		$posts	 = $this->client->getPosts(array('number' => 1));
+		$this->assertNotEmpty($posts);
+		$count	 = $this->guestClient->getCommentCount(1);
+	}
+
+	/**
+	 * @vcr comments/test-get-comment-vcr.yml
+	 */
+	public function testGetComment()
+	{
+		$posts		 = $this->client->getPosts(array('number' => 1));
+		$this->assertNotEmpty($posts);
+		$commentId	 = $this->client->newComment($posts[0]['post_id'], array('content' => 'Defacto 456'));
+		$comment	 = $this->client->getComment($commentId);
+		$this->assertArrayHasKey('comment_id', $comment);
+		$this->assertArrayHasKey('parent', $comment);
+		$this->assertArrayHasKey('user_id', $comment);
+		$this->assertArrayHasKey('date_created_gmt', $comment);
+		$this->assertArrayHasKey('status', $comment);
+		$this->assertArrayHasKey('content', $comment);
+		$this->assertArrayHasKey('link', $comment);
+		$this->assertArrayHasKey('post_id', $comment);
+		$this->assertArrayHasKey('post_title', $comment);
+		$this->assertArrayHasKey('author', $comment);
+		$this->assertArrayHasKey('author_url', $comment);
+		$this->assertArrayHasKey('author_email', $comment);
+		$this->assertArrayHasKey('author_ip', $comment);
+		$this->assertArrayHasKey('type', $comment);
+	}
+
+	/**
+	 * @vcr comments/test-get-comment-no-privilege-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 403
+	 * @expectedExceptionMessage You are not allowed to moderate comments on this site.
+	 */
+	public function testGetCommentNoPrivilege()
+	{
+		$comment = $this->guestClient->getComment(1);
+	}
+
+	/**
+	 * @vcr comments/test-get-comment-not-exist-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 404
+	 * @expectedExceptionMessage Invalid comment ID.
+	 */
+	public function testGetCommentNotExist()
+	{
+		$comment = $this->client->getComment(1000);
+	}
+
+	/**
+	 * @vcr comments/test-get-comment-no-filter-vcr.yml
+	 */
+	public function testGetCommentsNoFilter()
+	{
+		$posts		 = $this->client->getPosts(array('number' => 1));
+		$this->assertNotEmpty($posts);
+		$commentId	 = $this->client->newComment($posts[0]['post_id'], array('content' => 'Defacto 456 xyz!!!'));
+		$comments	 = $this->client->getComments();
+		$this->assertGreaterThan(0, count($comments));
+		$this->assertArrayHasKey('comment_id', $comments[0]);
+		$this->assertArrayHasKey('parent', $comments[0]);
+		$this->assertArrayHasKey('user_id', $comments[0]);
+		$this->assertArrayHasKey('date_created_gmt', $comments[0]);
+		$this->assertArrayHasKey('status', $comments[0]);
+		$this->assertArrayHasKey('content', $comments[0]);
+		$this->assertArrayHasKey('link', $comments[0]);
+		$this->assertArrayHasKey('post_id', $comments[0]);
+		$this->assertArrayHasKey('post_title', $comments[0]);
+		$this->assertArrayHasKey('author', $comments[0]);
+		$this->assertArrayHasKey('author_url', $comments[0]);
+		$this->assertArrayHasKey('author_email', $comments[0]);
+		$this->assertArrayHasKey('author_ip', $comments[0]);
+		$this->assertArrayHasKey('type', $comments[0]);
+	}
+
+	/**
+	 * @vcr comments/test-get-commnents-no-privilege-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 401
+	 * @expectedExceptionMessage Sorry, you cannot edit comments.
+	 */
+	public function testGetCommentsNoPrivilege()
+	{
+		$comments = $this->guestClient->getComments();
+	}
+
+	/**
+	 * @vcr comments/test-edit-comment-vcr.yml
+	 */
+	public function testEditComment()
+	{
+		$posts		 = $this->client->getPosts(array('number' => 1));
+		$this->assertNotEmpty($posts);
+		$commentId	 = $this->client->newComment($posts[0]['post_id'], array('content' => 'A comment to be edit'));
+		$this->assertGreaterThan(0, (int) $commentId);
+		$result		 = $this->client->editComment($commentId, array('content' => 'I have editted this comment!'));
+		$this->assertTrue($result);
+		$comment	 = $this->client->getComment($commentId);
+		$this->assertSame('I have editted this comment!', $comment['content']);
+	}
+
+	/**
+	 * @vcr comments/test-edit-comment-not-exist-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 404
+	 * @expectedExceptionMessage Invalid comment ID.
+	 */
+	public function testEditCommentNotExist()
+	{
+		$result = $this->client->editComment(1000, array('content' => 'I have editted this comment!'));
+	}
+
+	/**
+	 * @vcr comments/test-edit-comment-no-privilege-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 403
+	 * @expectedExceptionMessage You are not allowed to moderate comments on this site.
+	 */
+	public function testEditCommentNoPrivilege()
+	{
+		$result = $this->guestClient->editComment(1, array('content' => 'I have editted this comment!'));
+	}
+
+	/**
+	 * @vcr comments/test-delete-comment-vcr.yml
+	 */
+	public function testDeleteComment()
+	{
+		$posts		 = $this->client->getPosts(array('number' => 1));
+		$this->assertNotEmpty($posts);
+		$commentId	 = $this->client->newComment($posts[0]['post_id'], array('content' => 'A comment to be edit'));
+		$this->assertGreaterThan(0, (int) $commentId);
+		$result		 = $this->client->deleteComment($commentId);
+		$this->assertTrue($result);
+		$comment	 = $this->client->getComment($commentId);
+		$this->assertSame('trash', $comment['status']);
+	}
+
+	/**
+	 * @vcr comments/test-delete-comment-not-exist-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 404
+	 * @expectedExceptionMessage Invalid comment ID.
+	 */
+	public function testDeleteCommentNotExist()
+	{
+		$result = $this->client->deleteComment(1000);
+	}
+
+	/**
+	 * @vcr comments/test-delete-comment-no-privilege-vcr.yml
+	 * @expectedException HieuLe\WordpressXmlrpcClient\Exception\XmlrpcException
+	 * @expectedExceptionCode 403
+	 * @expectedExceptionMessage You are not allowed to moderate comments on this site.
+	 */
+	public function testDeleteCommentNoPrivilege()
+	{
+		$result = $this->guestClient->deleteComment(1);
+	}
+
+	/**
+	 * @vcr comments/test-get-comment-status-list-vcr.yml
+	 */
+	public function testGetCommentStatusList()
+	{
+		$statuses = $this->client->getCommentStatusList();
+		$this->assertGreaterThan(0, count($statuses));
+	}
+
 }
